97dc87a27f206df813e79e7cc1cbe6ed79bfa4e2
Jake Tronge <jtronge3@gmail.com>
2019-10-18 17:27:48 -0400

    Add config file

--- OOZero/__init__.py
+++ OOZero/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 
-# def create_app(config="OOZero.config.ProductionConfig"):
-def create_app(config="OOZero.config.DevelopmentConfig"):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_envvar('OOZERO_CONFIG')
 
-    app.secret_key = b'35f9aefeaaf1094198411e88c97a731675c2e6544c10cbfcd8392db338a292f1'
 
     from OOZero.model import db
     db.init_app(app)
